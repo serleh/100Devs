dac98964ea2eddd9a6a56e4795d67dd21c2a34bc
Saleh Abdullahi <salehabdullahi018@gmail.com>
2025-04-05 09:35:44 +0100

    built drink search app with cocktailDb API

--- APIs/cocktailDb/js/main.js
+++ APIs/cocktailDb/js/main.js
@@ -1,1 +1,19 @@
+document.querySelector("button").addEventListener("click", fetchData);
 
+function fetchData() {
+  choice = document.querySelector("input").value;
+  url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${choice}`;
+
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      document.querySelector("h4").innerText = data.drinks[0].strDrink;
+      document.querySelector("img").src = data.drinks[0].strDrinkThumb;
+      document.querySelector(".instruction").innerText =
+        data.drinks[0].strInstructions;
+    })
+    .catch((err) => {
+      console.log(`error: ${err}`);
+    });
+}
