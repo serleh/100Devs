ddf96e85be3332ed00a551498adf9684cdfa66a8
Saleh Abdullahi <salehabdullahi018@gmail.com>
2025-06-09 12:31:49 +0100

    node express reading review

--- node-express-reading/index.js
+++ node-express-reading/index.js
@@ -1,12 +1,5 @@
 const express = require("express");
 const app = express();
-const PORT = 3001;
-
-const generateId = () => {
-  const maxId =
-    notes.length > 0 ? Math.max(...notes.map((n) => Number(n.id))) : 0;
-  return String(maxId + 1);
-};
 
 let notes = [
   {
@@ -26,7 +19,14 @@ let notes = [
   },
 ];
 
+const generateId = () => {
+  const maxId =
+    notes.length > 0 ? Math.max(...notes.map((n) => Number(n.id))) : 0;
+  return String(maxId + 1);
+};
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("<h1>Hello World</h1>");
@@ -35,15 +35,20 @@ app.get("/", (req, res) => {
 app.get("/api/notes", (req, res) => {
   res.json(notes);
 });
+
 app.get("/api/notes/:id", (req, res) => {
   const id = req.params.id;
-  const note = notes.find((note) => note.id == id);
-  note ? res.json(note) : res.status(404).end();
+  const note = notes.find((note) => note.id === id);
+  if (note) {
+    res.json(note);
+  }
+  res.status(404).end();
 });
 
 app.delete("/api/notes/:id", (req, res) => {
   const id = req.params.id;
-  notes = notes.filter((note) => note.id !== id);
+  const note = notes.filter((note) => note.id === id);
+
   res.status(204).end();
 });
 
@@ -51,7 +56,7 @@ app.post("/api/notes", (req, res) => {
   const body = req.body;
 
   if (!body.content) {
-    return res.status(400).json({
+    return response.status(400).json({
       error: "content missing",
     });
   }
@@ -60,10 +65,12 @@ app.post("/api/notes", (req, res) => {
     important: body.important || false,
     id: generateId(),
   };
-  notes = notes.concat(note);
-  res.json(note);
-});
 
+  notes = notes.concat(note);
+
+  response.json(note);
+});
+const PORT = 3001;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
